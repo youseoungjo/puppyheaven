7fc5f1092a7111f0d6b23070a418357fecc2d202
syyyl <56541363+syyyl@users.noreply.github.com>
2023-04-20 11:37:37 +0900

    Create ProductData.js

---
+++ src/ProductData.js
@@ -0,0 +1,49 @@
+const ProductData = [
+    {
+        id: 1,
+        name: "대용량사료",
+        description: "This is product 1",
+        price: 159000,
+        image: "/images/ProductImage/대용량사료.png",
+        isFavorited: false,
+        category : "category1",
+      },
+      {
+        id: 2,
+        name: "아침애사료",
+        description: "This is product 2",
+        price: 30030,
+        image: "/images/ProductImage/아침애사료.png",
+        isFavorited: true,
+        category : "category2",
+      },
+      {
+        id: 3,
+        name: "청소년사료",
+        description: "This is product 3",
+        price: 142000,
+        image: "/images/ProductImage/청소년사료.png",
+        isFavorited: true,
+        category : "category3",
+      },
+      {
+        id: 4,
+        name: "체중관리사료",
+        description: "This is product 4",
+        price: 29110,
+        image: "/images/ProductImage/체중관리사료.png",
+        isFavorited: false,
+        category : "category4",
+      },
+      {
+        id: 5,
+        name: "특수견사료",
+        description: "This is product 5",
+        price: 41900,
+        image: "/images/ProductImage/특수견사료.png",
+        isFavorited: false,
+        category : "category5",
+      },
+]
+
+export default ProductData;
