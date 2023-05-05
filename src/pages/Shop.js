c1b3207bf051ea926006fb8a2af2315e921c2d48
[SW4/1반] 김예은 <christinaye0403@gmail.com>
2023-05-05 21:25:57 +0900

    css 적용 가능하게 수정

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -85,76 +85,75 @@ const Shop = ({ wishItem, setWishItem }) => {
   return (
     <div className="Shop">
 
-        <div className="Logo">
-          <img src="shortlogo.png" alt="로고 이미지" className="logo-image"></img>
-        </div>
 
-        <div className="Shop-content">
+      <div className="Logo">
+        <img src="shortlogo.png" alt="로고 이미지" className="logo-image"></img>
+      </div>
+
+      <div className="Shop-content">
           <div className="Category">
             <div className="list-group list-group-flush">
-              <div style={{ margin: '10px' }} />
-              <button type="button" className="list-group-item" onClick={() => navigate('/main')}>
-              메인화면
-              </button>
-              <button type="button" className="list-group-item" onClick={() => navigate('/shop')}>
-              계속 쇼핑하기
-              </button>
-              <button type="button" className="list-group-item" onClick={() => navigate('/wish')}>
-              위시리스트
-              </button>
-              <div style={{ margin: '30px' }} />
-              <section>
-                <div className="list-group-item">
-                <button className="cate_btn" onClick={() => categoryFilterResult('all')}>
-                all
-                </button>
-                </div>
-                <div className="list-group-item">
-                <button className="cate_btn" onClick={() => categoryFilterResult('1')}>
-                애견 사료
-                </button>
-                </div>
-                <div className="list-group-item">
-                <button className="cate_btn" onClick={() => categoryFilterResult('2')}>
-                애견 장난감
-                </button>
-                </div>
-                <div className="list-group-item">
-                <button className="cate_btn" onClick={() => categoryFilterResult('3')}>
-                카테고리3
-                </button>
-                </div>
-                <div className="list-group-item">
-                <button className="cate_btn" onClick={() => categoryFilterResult('4')}>
-                카테고리4
-                </button>
-                </div>
-                <div className="list-group-item">
-                <button className="cate_btn" onClick={() => categoryFilterResult('5')}>
-                카테고리5
-                </button>
-                </div>
-              </section>
+                <div style={{margin: "10px"}}/>
+                <button type="button" className="list-group-item" onClick={()=>navigate('/main')}>메인화면</button>
+                <button type="button" className="list-group-item" onClick={()=>navigate('/shop')}>계속 쇼핑하기</button>
+                <button type="button" className="list-group-item" onClick={()=>navigate('/cart')}>장바구니</button>
+                <div style={{margin: "30px"}}/>
+                <section>
+                    <div className="list-group-item">
+                      <button className="cate_btn" onClick={() => categoryFilterResult('all')}>
+                      all
+                      </button>
+                    </div>
+                    <div className="list-group-item">
+                      <button className="cate_btn" onClick={() => categoryFilterResult('1')}>
+                      애견 사료
+                      </button>
+                    </div>
+                    <div className="list-group-item">
+                      <button className="cate_btn" onClick={() => categoryFilterResult('2')}>
+                      애견 장난감
+                      </button>
+                    </div>
+                    <div className="list-group-item">
+                      <button className="cate_btn" onClick={() => categoryFilterResult('3')}>
+                      카테고리3
+                      </button>
+                    </div>
+                    <div className="list-group-item">
+                      <button className="cate_btn" onClick={() => categoryFilterResult('4')}>
+                      카테고리4
+                      </button>
+                    </div>
+                    <div className="list-group-item">
+                      <button className="cate_btn" onClick={() => categoryFilterResult('5')}>
+                      카테고리5
+                      </button>
+                    </div>
+                </section>
             </div>
           </div>
-        </div>
 
-      <div className="product_list">
-        <ProductList
-          productData={productdatas}
-          handleFavoriteClick={handleFavoriteClick}
-          handleAddWish={handleAddWish}
-          handleRemoveWish={handleRemoveWish}
-          wishItem={wishItem}
-        />
+
+          <div className="product-list">
+
+              <ProductList
+                productData={productdatas}
+                handleFavoriteClick={handleFavoriteClick}
+                handleAddWish={handleAddWish}
+                handleRemoveWish={handleRemoveWish}
+                wishItem={wishItem}
+              />
+
+          </div>
+
+          <div className="product-compare-list">
+
+            <ProductCompareList />
+
+          </div>
       </div>
-      <div className="product-compare-list">
-
-              <ProductCompareList />
-
-      </div>
-    </div>
-  );
-};
+  </div>
+)};
 
 export default Shop;
+
