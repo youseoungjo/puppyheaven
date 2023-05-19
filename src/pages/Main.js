68ca74963fed37e2bc712c290c0d23ffd2b38898
[SW4/1반] 김예은 <christinaye0403@gmail.com>
2023-05-19 20:23:33 +0900

    메인화면 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,15 +13,25 @@ const Main = () => {
     };
     return (
       <div className="Main">
-          <div className="main-logo">
-            <img src="shortlogo.png" alt="로고 이미지" className="logo-image"/>
+          <div className="main-logo-box">
+            <div className="main-logo">
+              <img src="shortlogo.png" alt="로고 이미지" className="logo-image"/>
+              <span>PUPPY HEAVEN</span>
+            </div>
+
             <div className="logout-btn" onClick={handleLogout}>
               <img src="logoutv2.png" alt="로그아웃" className="logout-img" />
-              <span>로그 아웃</span>
+              <div>로그 아웃</div>
             </div>
           </div>
+
+          <div className="main-text">
+            <span>For Better Purchase</span><br/>
+            <span>For Better Healthy Life</span><br/>
+            <span className="korean">퍼피해븐은 애견인들을 위해 최선을 다하겠습니다.</span>
+          </div>
       
-          <div>
+          <div className="main-btn-box">
             <button className="main-btn" onClick={()=>navigate('/map')}> <img src="map.gif" alt="map" /> <span>PUPPY MAP</span> </button>
             <button className="main-btn" onClick={()=>navigate('/shop')}> <img src="shop.gif" alt="GIF 이미지" /> <span>PUPPY SHOP</span></button>
           </div>
