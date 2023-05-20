86486dbe1fccdaf52d1330127d3ec557ef63511d
syyyl <56541363+syyyl@users.noreply.github.com>
2023-05-20 22:32:42 +0900

    Update Shop.js

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -140,7 +140,7 @@ const handleRemoveWish = async (id) => {
           <div className="Category">
             <div className="list-group list-group-flush">
                 <div style={{margin: "10px"}}/>
-                  <button type="button" className="list-group-item" onClick={() => navigate('/main')}>메인화면</button>
+                  <button type="button" className="list-group-item" onClick={() => navigate('/')}>메인화면</button>
                   <button type="button" className="list-group-item" onClick={() => navigate('/shop')}>계속 쇼핑하기</button>
                   <button type="button" className="list-group-item" onClick={() => navigate('/wish')}>위시리스트</button>
                   <div style={{margin: "30px"}}/>
